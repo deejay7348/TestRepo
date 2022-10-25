d8975d3edff677d01f1448138fce079c1bef6917
deejayy2k@gmail.com <dee_jayy2k@yahoo.com>
2022-10-25 11:59:52 +0300

    updates

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+"""
+System description
+*Streets have signs to make drivers aware of the speed limits.
+    If i have a LIST of speed ranges then I would use a python list.
+
+    Speed ranges are: []
+        
+
+
+"""
